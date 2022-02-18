7a2e6a3e73e58f0e011d406a6e28a7097b53354a
Theethawat Savastham <theethawat@theduckcreator.in.th>
2022-02-18 22:27:05 +0700

    New React

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,10 +1,10 @@
-import Mongoose from "mongoose";
-import hash from "object-hash";
-import uniqueValidator from "mongoose-unique-validator";
+import Mongoose from 'mongoose';
+import hash from 'object-hash';
+import uniqueValidator from 'mongoose-unique-validator';
 
-import config from "../configs/app";
+import config from '../configs/app';
 
-const Schema = Mongoose.Schema;
+const { Schema } = Mongoose;
 
 const UserSchema = new Schema({
   id: String,
@@ -15,32 +15,28 @@ const UserSchema = new Schema({
     unique: true,
   },
   password: String,
-  role: { type: String, default: "USER" },
+  role: { type: String, default: 'USER' },
 });
 
 UserSchema.plugin(uniqueValidator);
 
-UserSchema.methods.passwordHash = (password) => {
-  return hash.sha1({
+UserSchema.methods.passwordHash = (password) =>
+  hash.sha1({
     secret: config.secret,
-    password: password,
+    password,
   });
-};
 
-UserSchema.methods.validPassword = (password, userObj) => {
-  return (
-    hash.sha1({
-      secret: config.secret,
-      password: password,
-    }) === userObj.password
-  );
-};
+UserSchema.methods.validPassword = (password, userObj) =>
+  hash.sha1({
+    secret: config.secret,
+    password,
+  }) === userObj.password;
 
-UserSchema.pre("save", function (next) {
+UserSchema.pre('save', function (next) {
   this.password = this.passwordHash(this.password);
   next();
 });
 
-const UserModel = Mongoose.model("User", UserSchema);
+const UserModel = Mongoose.model('User', UserSchema);
 
 export default UserModel;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: 'kanit',
+      },
+      colors: {
+        tdc: {
+          50: '#32ffe4',
+          100: '#28f9da',
+          200: '#1eefd0',
+          300: '#14e5c6',
+          400: '#0adbbc',
+          500: '#00d1b2',
+          600: '#00c7a8',
+          700: '#00bd9e',
+          800: '#00b394',
+          900: '#00a98a',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
