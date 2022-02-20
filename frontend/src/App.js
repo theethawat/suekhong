517e649e60c37dc1282bf9cf23c4f85205d7631a
Theethawat Savastham <theethawat@theduckcreator.in.th>
2022-02-20 23:07:55 +0700

    วาง Layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,35 @@
+/* eslint-disable react/jsx-wrap-multilines */
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Routes, BrowserRouter, Route } from 'react-router-dom'
+import { Auth } from './views'
+import './index.css'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <BrowserRouter>
+        {window.localStorage.getItem('TDC_TOKEN') ? (
+          <Routes>
+            <Route path="management">
+              <Route path="user" element={<div>Management User</div>} />
+              <Route path="user" element={<div>Management User</div>} />
+            </Route>
+            <Route path="dashboard">
+              <Route index element={<div>dashboard index</div>} />
+            </Route>
+            <Route index element={<div>Home</div>} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route>
+              <Route path="login" element={<div>Login</div>} />
+              <Route index element={<Auth.Welcome />} />
+            </Route>
+          </Routes>
+        )}
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
---
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -0,0 +1,70 @@
+/* This example requires Tailwind CSS v2.0+ */
+import React from 'react'
+import { Disclosure } from '@headlessui/react'
+import PropTypes from 'prop-types'
+
+export default function AuthLayout({ title, children }) {
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-100">
+        <body class="h-full">
+        ```
+      */}
+      <div className="min-h-full">
+        <Disclosure as="nav" className="bg-gray-100">
+          {({ open }) => (
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex items-center justify-between h-16">
+                <div className="flex items-center gap-2">
+                  <div className="flex-shrink-0">
+                    <img
+                      className="h-8 w-8"
+                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                      alt="Workflow"
+                    />
+                  </div>
+                  <div className="text-xl font-display font-semibold">
+                    Bangriang
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </Disclosure>
+
+        <header className="bg-white shadow">
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-900">{title}</h1>
+          </div>
+        </header>
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            {/* Replace with your content */}
+            <div className="px-4 py-6 sm:px-0">
+              <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
+                {children}
+              </div>
+            </div>
+            {/* /End replace */}
+          </div>
+        </main>
+      </div>
+    </>
+  )
+}
+
+AuthLayout.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+}
+
+AuthLayout.defaultProps = {
+  title: '',
+}
